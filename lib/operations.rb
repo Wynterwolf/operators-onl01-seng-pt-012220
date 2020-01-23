5a3640e9734124332ce4fbe4b365ff9310d10926
Samantha Jordan <shinyguns@gmail.com>
2020-01-23 17:24:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,14 @@
 def unsafe?(speed)
   if speed < 40 && > 60
-    "safe"
+    true
   else
-    "not safe"
+    false
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 && > 60 ? "safe" : "not safe"
+	speed < 40 && > 60 ? true : false
 end
 	
 
